7f1d96c5a7b006789316ef1a2c6ed7de2749899d
takashiono2 <takashiono710@gmail.com>
2021-07-26 16:14:18 +0900

    5

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
 'use strict';
 
+// #5
 {
-  // const score1 = 80;
-  // const score2 = 90;
-  // const score3 = 40;
+  const scores = [80, 90, 40, 70];
+  scores.splice(1,1,40,50);
+  //80,40,50,40,70
 
-  const scores = [80, 90, 40];
-  console.log(scores);
+  for(var i=0; i < scores.length ;i++){
+    console.log(`スコア ${i}:${scores[i]}`);
+    }
 }
+
+
+// {
+//   const scores = [80, 90, 40, 70];
+//   scores.push(60,70);
+//   scores.shift();
+
+//   for(var i=0; i < scores.length ;i++){
+//     console.log(`スコア ${i}:${scores[i]}`);
+//     }
+// }
+
+// #1
+// {
+//   // const score1 = 80;
+//   // const score2 = 90;
+//   // const score3 = 40;
+
+//   const scores = [80, 90, 40];
+//   console.log(scores);
+// }
