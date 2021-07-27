33fdd0da4b5692caf4910426e0c2534a0e9be951
takashiono2 <takashiono710@gmail.com>
2021-07-27 10:41:39 +0900

    7

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
 'use strict';
 
-// #6
+// #7
 {
-  const otherScores = [10,20];
-  const scores = [80, 90, 40, 70, ...otherScores];
-  console.log(scores);
-  sum(...otherScores);
+  // const scores = [80, 90, 40, 70]
+  // const[a,b,c,d] = [80, 90, 40, 70]
+  // console.log(a);
+  // console.log(b);
+  // console.log(c);
+  // console.log(d);
+  
+  // const[a,b, ...others] = scores;
+  // console.log(a);
+  // console.log(b);
+  // console.log(others);
+
+  let x = 30;
+  let y = 70;
+  [x,y] = [y,x];
+  console.log(x);
+  console.log(y);
 }
+
+// #6
+// {
+//   const otherScores = [10,20];
+//   const scores = [80, 90, 40, 70, ...otherScores];
+//   console.log(scores);
+//   sum(...otherScores);
+// }
 
 // #5
 // {
